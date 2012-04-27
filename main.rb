53f9fbe0baba8d56ca3cc5959608126b14455821
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2012-04-27 16:27:19 +1200

    Google search link

--- main.rb
+++ main.rb
@@ -2,10 +2,20 @@ $stdout.sync = true
 
 require 'bundler'
 require 'sinatra'
+require 'cgi'
 
 set :haml, :format => :html5
 
+def googleSearchUrl(query)
+  return "http://www.google.com/search?as_q=#{CGI.escape(query)}"
+end
+
+def spacified(text)
+  return text
+end
+
 get '/' do
   @address = params[:address]
+  @googleSearchUrl = googleSearchUrl(spacified(@address))
   haml :index
 end
